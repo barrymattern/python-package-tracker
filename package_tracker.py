e24f7a47bcfd136a799753b7f6f541edefc513f4
giiaga <ghousali99@gmail.com>
2021-01-14 16:41:33 -0500

    blah

--- package_tracker.py
+++ package_tracker.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from config import Config
 from routes import main
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(main.bp)
+db.init_app(app)
+migrate = Migrate(app, db)
