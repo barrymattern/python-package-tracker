34896f66ef09a495b777e61b62c4c448751e36c5
giiaga <ghousali99@gmail.com>
2021-01-14 14:09:23 -0500

    Form setting up not complete

---
+++ app/shipping_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, BooleanField, SubmitField
+from map.map import map
+from wtforms.validators import DataRequired
+
+
+class Shipping_Form(FlaskForm):
+    sender = StringField('Sender', validators=[DataRequired()])
+    recipient = StringField('Recipient', validators=[DataRequired()])
+    origin = SelectField('Origin', choices=[tuple((key, key)) for key in map.keys()], validators=[DataRequired()])
+    destination = SelectField('Destination', choices=[tuple((key, key)) for key in map.keys()], validators=[DataRequired()])
+    express = BooleanField('Express Shipping', default=False)
+    submit = SubmitField('Submit')
+    cancel = SubmitField('Cancel')
--- routes/main.py
+++ routes/main.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 bp = Blueprint('main', __name__, url_prefix='')
 
 @bp.route('/')
 def packageTracker():
     return 'Package Tracker'
+
+@bp.route('/new_package', methods=['GET', 'POST'])
+def newPackage():
+    return render_template('shipping_request.html')
